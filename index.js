00b5c32da8ff5b42f8f819c31c535b2c86f8e287
Juan Monetta <jpmonettas@gmail.com>
2020-01-10 16:11:35 -0300

    Reproducing bug

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var firstAccount = "0xe9fc7a1f53f6a5995835ed195c45dcdffc63a7a1";
 var contractAddress = "0x2217BbB537Dd857733634E28b4a76955f62a97a1";
 var abi = (JSON.parse(fs.readFileSync("./contracts/build/MyContract.json"))["abi"]);
 
-var instance = new web3.eth.Contract(abi, contractAddress, {from: firstAccount});
-instance.methods.fireSomeEvent("0xA").send();
+var instance = new web3.eth.Contract(abi, contractAddress);
+instance.methods.fireSomeEvent(0).send({from: firstAccount,
+                                        gas: 4000000
+                                       });
+instance.methods.fireSomeEvent(0).send({from: firstAccount,
+                                        gas: 4000000
+                                       });
 console.log("Done!");
